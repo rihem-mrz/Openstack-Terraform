e0c7639e698eff223b99a864bf4e136e79c40b6f
rihem-mrz <rihem.marzougui@supcom.tn>
2022-09-02 04:45:46 -0400

    single VM

---
+++ main.tf
@@ -0,0 +1,94 @@
+#define required providers
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+#define provider access credentials
+provider "openstack" {
+  user_name   = var.user_name  #tenant_name = "admin"
+  password    = var.password
+  auth_url    = var.auth_url
+  region      = var.region
+  tenant_name = "admin"
+  domain_id   = "default"
+}
+
+
+#provide ssh key to access instance
+resource "openstack_compute_keypair_v2" "my-node-key" {
+  name       = var.key_name
+  public_key = var.ssh_key
+}
+
+
+#define network and subnet 
+resource "openstack_networking_network_v2" "network" {
+  name           = var.network_name
+  admin_state_up = "true"
+}
+resource "openstack_networking_subnet_v2" "subnet" {
+  name       = var.subnet_name
+  network_id = "${openstack_networking_network_v2.network.id}"
+  cidr       = "192.168.101.0/24"
+  ip_version = 4
+}
+
+
+#define router and router interface 
+resource "openstack_networking_router_v2" "router" {
+  name             = var.router_name
+  admin_state_up   = "true"
+}
+resource "openstack_networking_router_interface_v2" "router-interface" {
+  router_id = "${openstack_networking_router_v2.router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
+}
+
+
+#security group add rules accessing instances
+resource "openstack_compute_secgroup_v2" "secgroup" {
+  name        = var.secgroup_name
+  description = "Security group for instances"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+
+#ip flowting to get a public address for external connections 
+#resource "openstack_compute_floatingip_v2" "floatip" {
+#  pool = var.pool
+#}
+
+
+#create Openstack instance
+resource "openstack_compute_instance_v2" "node" {
+  name            = var.instance_name
+  image_name      = var.image
+  flavor_name     = var.flavor
+  key_pair        = "${openstack_compute_keypair_v2.my-node-key.name}"
+  security_groups = ["${openstack_compute_secgroup_v2.secgroup.name}"]
+
+  network {
+    uuid        = "${openstack_networking_network_v2.network.id}"
+    fixed_ip_v4 = "192.168.101.43"
+  }
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "user_name" {
+    description = "The username to be used for authentication in Openstack"
+    type        = string
+}
+
+variable "password" {
+    description = "The password to be used for authentication in Openstack"
+    type        = string
+}
+
+variable "region" {
+    description = "The Openstack region for provisioned VMs"
+    type        = string
+    default     = "RegionOne"
+}
+
+variable "auth_url" {
+    description = "The URL for Openstack"
+    type        = string
+}
+
+variable "key_name" {
+    description = "Name for SSH key"
+    type        = string
+}
+
+variable "ssh_key" {
+    description = "SSH key to access Openstack instance"
+    type        = string
+}
+
+variable "network_name" {
+    description = "Name of the network"
+    type        = string
+}
+
+variable "subnet_name" {
+    description = "Name of the subnet"
+    type        = string
+}
+
+variable "router_name" {
+    description = "Name of the router"
+    type        = string
+}
+
+variable "secgroup_name" {
+    description = "Name of the security group"
+    type        = string
+}
+
+variable "pool" {
+    description = "Define the pool where it belong"
+    type        = string
+    default     = "provider"
+}
+
+variable "instance_name" {
+    description = "Name of the instance"
+    type        = string
+}
+
+variable "image" {
+    description = "The Os image for the instance"
+    type        = string
+}
+
+variable "flavor" {
+    description = "define the flavor to get the hardware description for instances"
+    type        = string
+    default     = "m1.small"
+}
